70ea3aa5c1e564596c0d946696ea905439cdf462
Muhammad Aqib <mohammadaaqib77@gmail.com>
2024-12-15 23:22:28 +0500

    adding query params

--- 04-storee-api/controllers/products.js
+++ 04-storee-api/controllers/products.js
@@ -1,12 +1,29 @@
+const Product = require("../models/product");
+
 const getAllProducts = async (req, res) => {
-  res.status(200).send("Get all product");
+  const { featured, name, company } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name =  { $regex: name, $options: 'i' }
+  }
+
+  const products = await Product.find(queryObj);
+  res.status(200).json({ total: products.length, products });
 };
 
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).send("Get all product Statics");
+  const products = await Product.find({});
+  res.status(200).json({ products });
 };
 
-module.exports={
-    getAllProducts,
-    getAllProductsStatic
-}
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
