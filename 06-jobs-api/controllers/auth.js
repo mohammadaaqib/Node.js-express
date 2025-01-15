e24f1f92d2f23621b592920687ee62090e86aba1
Muhammad Aqib <mohammadaaqib77@gmail.com>
2025-01-15 23:16:03 +0500

    regester and db connect done

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,8 +1,25 @@
-const regester=async(req,res)=>{
-    res.send('register route');
-}
-const login=async(req,res)=>{
-    res.send('login route');
-}
+const { use } = require("express/lib/router");
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
 
-module.exports={login,regester}
+const bcrypt = require("bcryptjs");
+
+const regester = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const tempUser = { name, email, password: hashedPassword };
+
+    const user = await User.create({ ...tempUser });
+    res.status(StatusCodes.CREATED).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const login = async (req, res) => {
+  res.send("login route");
+};
+
+module.exports = { login, regester };
---
+++ 06-jobs-api/models/User.js
@@ -0,0 +1,27 @@
+const { required } = require('joi');
+const mongoose=require('mongoose');
+
+const User=new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        maxlength: 50,
+        minlength: 3,
+      },
+      email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        match: [
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          'Please provide a valid email',
+        ],
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: [true, 'Please provide password'],
+        minlength: 6,
+      },
+})
+
+module.exports=mongoose.model('User',User)
