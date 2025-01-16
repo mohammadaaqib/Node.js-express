d1826bc82a556d03198389f7a33ecb9fc39825d9
Muhammad Aqib <mohammadaaqib77@gmail.com>
2025-01-16 22:40:19 +0500

    login fucntionality is done

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,19 +1,33 @@
 const User = require("../models/User");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
 
 const regester = async (req, res) => {
-    try {
-        const user = await User.create({ ...req.body });
-        const token = user.createJWT();
-        res.status(StatusCodes.CREATED).json({ user:user.name, token});
-    } catch (error) {
-        console.log(error);
-    }
-
+  try {
+    const user = await User.create({ ...req.body });
+    const token = user.createJWT();
+    res.status(StatusCodes.CREATED).json({ user: user.name, token });
+  } catch (error) {
+    console.log(error);
+  }
 };
 const login = async (req, res) => {
-  res.send("login route");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+  const passwordCorrect = await user.matchPasswords(password);
+  
+  if (!passwordCorrect) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: user.name, token });
 };
 
 module.exports = { login, regester };
