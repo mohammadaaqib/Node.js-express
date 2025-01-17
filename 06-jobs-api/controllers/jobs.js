d78655b1c5c784f91fc07b8181700dacf3cb47eb
Muhammad Aqib <mohammadaaqib77@gmail.com>
2025-01-17 22:48:06 +0500

    create job is done

--- 06-jobs-api/controllers/jobs.js
+++ 06-jobs-api/controllers/jobs.js
@@ -1,18 +1,24 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs');
-}
+  res.send("get all jobs");
+};
 const getJob = async (req, res) => {
-    res.send('get single job');
-}
-const createJob = async (req, res) => { 
-    res.json(req.user);
-}
+  res.send("get single job");
+};
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+res.status(StatusCodes.CREATED).json({job})
+  
+};
 const updateJob = async (req, res) => {
-    res.send('update job');
-}   
+  res.send("update job");
+};
 const deleteJob = async (req, res) => {
-    res.send('delete job');
-}
+  res.send("delete job");
+};
 
-
-module.exports = {getAllJobs, getJob, createJob, updateJob, deleteJob};
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
